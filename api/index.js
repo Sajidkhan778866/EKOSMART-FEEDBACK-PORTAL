c34e9c664d72c1404cc145c9a8e4e3be8c88be8b
Sajidkhan778866 <sajidkhan778866@users.noreply.github.com>
2026-09-26 09:53:45 +0530

    fix(vercel): remove conflicting api/index.js files to resolve conflicting path names in Vercel build

--- api/index.js
+++
@@ -1,5 +0,0 @@
-const app = require('../backend/dist/app').default || require('../backend/dist/src/app').default || require('../backend/dist/app');
-
-module.exports = (req, res) => {
-  return app(req, res);
-};
--- backend/api/index.js
+++
@@ -1,5 +0,0 @@
-const app = require('../dist/app').default || require('../dist/src/app').default || require('../dist/app');
-
-module.exports = (req, res) => {
-  return app(req, res);
-};
